5763e957a933ebf8f0551056dfcb9e692cf943f8
molhot <sakata19991214@gmail.com>
2024-10-01 20:49:32 +0900

    add serializer and regist api url to root

---
+++ ledger_app_project/api/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from ledger_app.models import UserModel, ItemModel
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('id', 'user_name','user_address', 'user_password')
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemModel
+        fields = ('id', 'item_name', 'item_user_id', 'item_price')
+
--- ledger_app_project/handle_db_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HandleDbAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'handle_db_app'
