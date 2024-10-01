48fbca1fe6ea43edcf74b2688c7f03a7e6db0e9c
molhot <sakata19991214@gmail.com>
2024-10-01 14:04:26 +0900

    add item model

---
+++ ledger_app_project/handle_db_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HandleDbAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'handle_db_app'
---
+++ ledger_app_project/ledger_app/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+class UserModel(models.Model):
+    user_name = models.CharField(
+        blank = True,
+        max_length = 255,
+    )
+
+    user_address = models.CharField(
+        blank = False,
+        max_length = 255,
+    )
+
+    user_password = models.CharField(
+        blank = False,
+        max_length = 255,
+    )
+
+    def __str__(self):
+        return f'{self.user_name}'
+
+class ItemModel(models.Model):
+    item_name = models.CharField(
+        blank = False,
+        max_length = 255,
+    )
+
+    item_user_id = models.ForeignKey(
+        UserModel, 
+        on_delete=models.CASCADE
+    )
+
+    item_price = models.IntegerField(
+        blank = False,
+        max_length = 255,
+    )
+
+    def __str__(self):
+        return f'{self.item_name} is {self.item_price}'
